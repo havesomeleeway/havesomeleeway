9f628bff83587b4af68e6e026f66197cff846438
leewei <leewei@live.com.sg>
2019-01-12 15:53:24 +0800

    added redirect

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -6,8 +6,8 @@ export default () => (
         <section className="section">
           <div className="container">
             <div className="content">
-                <h1>Thank you!</h1>
-                <p>This is a custom thank you page for form submissions</p>
+                <h1>Thank you for getting in touch</h1>
+                <p>I'll get back to you asap!</p>
             </div>
           </div>
           </section>
